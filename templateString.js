27824fc6f7075ebc00a969f3d2d4ec4a946c8059
Muhammad Alinoor <alinoor145387@outlook.com>
2021-08-23 12:03:01 +0600

    multiline-string-in-carret-and-dollarBrace-for-variable-in-carret

---
+++ templateString.js
@@ -0,0 +1,7 @@
+// multiline string is declared inside of a carret=backtick=backquote sign.
+const first = "labib";
+const second = "Chowdhury";
+// now will declare variable within second bracket in backtick
+const fullNew = `${first} ${second}`;
+console.log(fullNew);
+console.log(`${fullNew}`);
